4288d06a9311e04e6de9ca21d60d7e02997a6104
mahdy-nasr <mahdy.m.nasr@gmail.com>
2018-04-04 22:09:31 +0300

    fix static view issue and correct the controller :)

--- Controller/Subscriber/NewAction.php
+++ Controller/Subscriber/NewAction.php
@@ -15,7 +15,6 @@ use MahdyNasr\Newsletter\Model\NewsletterCookie;
 use MahdyNasr\Newsletter\Model\NewsletterCookieFactory;
 
 
-
 class NewAction extends \Magento\Framework\App\Action\Action
 {
     /**
@@ -72,55 +71,57 @@ class NewAction extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-        if ($this->getRequest()->isPost() && $this->getRequest()->getPost('email')) {
-
-            $email = (string)$this->getRequest()->getPost('email');
-            $first_name = $this->getRequest()->getPost('name');
-
-            try {
-                $this->validateEmailFormat($email);
-
-                // check if subscriber is already subscribed
-                $subscriber = $this->subscriberFactory->create()->loadByEmail($email);
-
-                if ($subscriber->getId()
-                    && $subscriber->getSubscriberStatus() == Subscriber::STATUS_SUBSCRIBED
-                ) {
-                    throw new \Magento\Framework\Exception\LocalizedException(
-                        __('This email address is already subscribed.')
-                    );
-                }
-
-
-
-                $status = $this->subscriberFactory->create()
-                    ->setFirstName($first_name)
-                    ->subscribe($email);
-
-                // set the subscription status on cookies
-                $this->newsletterCookie->set($status);
-
-                if ($status == Subscriber::STATUS_NOT_ACTIVE) {
-                    $this->messageManager->addSuccess(__('The confirmation request has been sent.'));
-                } else {
-                    $this->messageManager->addSuccess(__('Thank you for your subscription.'));
-                }
-
-            } catch (\Magento\Framework\Exception\LocalizedException $e) {
-                $this->messageManager->addException(
-                    $e,
-                    __('There was a problem with the subscription: %1', $e->getMessage())
-                );
-            } catch (\Exception $e) {
-
-                // set cookie in case the problem was from mail sender or something like that.
-                if ($this->subscriberFactory->create()->loadByEmail($email)->isSubscribed()) {
-                    $this->newsletterCookie->set(Subscriber::STATUS_SUBSCRIBED);
-                }
-
-                $this->messageManager->addException($e, __('Something went wrong with the subscription.'));
-            }
+        if (!$this->getRequest()->isPost() && $this->getRequest()->getPost('email')) {
+            return $this->resultFactory->create(ResultFactory::TYPE_REDIRECT)
+                ->setUrl($this->_redirect->getRedirectUrl());
         }
+
+        $email = (string)$this->getRequest()->getPost('email');
+        $first_name = $this->getRequest()->getPost('name');
+
+        try {
+            $this->validateEmailFormat($email);
+
+            // check if subscriber is already subscribed
+            $subscriber = $this->subscriberFactory->create()->loadByEmail($email);
+
+            if ($subscriber->getId()
+                && $subscriber->getSubscriberStatus() == Subscriber::STATUS_SUBSCRIBED
+            ) {
+                throw new \Magento\Framework\Exception\LocalizedException(
+                    __('This email address is already subscribed.')
+                );
+            }
+
+
+            $status = $this->subscriberFactory->create()
+                ->setFirstName($first_name)
+                ->subscribe($email);
+
+            // set the subscription status on cookies
+            $this->newsletterCookie->set($status);
+
+            if ($status == Subscriber::STATUS_NOT_ACTIVE) {
+                $this->messageManager->addSuccess(__('The confirmation request has been sent.'));
+            } else {
+                $this->messageManager->addSuccess(__('Thank you for your subscription.'));
+            }
+
+        } catch (\Magento\Framework\Exception\LocalizedException $e) {
+            $this->messageManager->addException(
+                $e,
+                __('There was a problem with the subscription: %1', $e->getMessage())
+            );
+        } catch (\Exception $e) {
+
+            // set cookie in case the problem was from mail sender or something like that.
+            if ($this->subscriberFactory->create()->loadByEmail($email)->isSubscribed()) {
+                $this->newsletterCookie->set(Subscriber::STATUS_SUBSCRIBED);
+            }
+
+            $this->messageManager->addException($e, __('Something went wrong with the subscription.'));
+        }
+
 
         return $this->resultFactory->create(ResultFactory::TYPE_REDIRECT)
             ->setUrl($this->_redirect->getRedirectUrl());
